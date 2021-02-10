336592318cc2b279a67a82539a24eede44ceb546
Gustavo Boni <gustavo.boni@gmail.com>
2021-02-10 09:23:52 -0300

    starting grid system

---
+++ Assets/Scripts/GemGrid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemGrid : MonoBehaviour, ISerializationCallbackReceiver
+{
+    public int height;
+    public int width;
+
+    int[,] grid;
+
+    public void OnAfterDeserialize()
+    {
+        grid = new int[width, height];
+    }
+
+    public void OnBeforeSerialize()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
