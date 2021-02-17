2b41f2b8b4350bea922e38e880257ef56671eefd
Gustavo Boni <gustavo.boni@gmail.com>
2021-02-17 12:15:59 -0300

    code clean up

--- Assets/Scripts/GridSystem.cs
+++ Assets/Scripts/GridSystem.cs
@@ -4,68 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-//stores the current selected gem
-public class Move
-{
-    public List<Gem> selection;
-
-    int numOfMovingGems = 2;
-
-    public UnityEvent OnMoveEnd = new UnityEvent();
-
-    public Move()
-    {
-        selection = new List<Gem>();
-    }
-
-    public void AddSelection(Gem g)
-    {
-        g.OnMovementEnd.AddListener(OnGemMovementEnd);
-        selection.Add(g);
-    }
-
-    void OnGemMovementEnd(Gem g)
-    {
-        g.OnMovementEnd.RemoveAllListeners();
-
-        numOfMovingGems--;
-        if (numOfMovingGems == 0)
-        {
-            numOfMovingGems = 2;
-            OnMoveEnd?.Invoke();
-        }
-
-    }
-
-    public bool IsFirstSelection()
-    {
-        return selection.Count > 0 ? false : true;
-    }
-
-    public void Swap()
-    {
-        if(selection.Count > 0)
-        {
-            selection[0].MoveTo(selection[1].transform.position, 0.3f);
-            selection[1].MoveTo(selection[0].transform.position, 0.3f);
-
-            //setting gem to his new cell
-            Cell aux;
-            aux = selection[0].currentCell;
-            selection[0].SetCell(selection[1].currentCell);
-            selection[0].currentCell.SetGem(selection[0]);
-            selection[1].SetCell(aux);
-            selection[1].currentCell.SetGem(selection[1]);
-        }
-    }
-
-    public void Reset()
-    {
-        numOfMovingGems = 2;
-        selection.Clear();
-    }
-}
-
 //Organizes all gems and keep the current move
 public class GridSystem : MonoBehaviour
 {
