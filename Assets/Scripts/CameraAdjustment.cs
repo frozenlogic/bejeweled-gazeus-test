3115cb53fab13b1640435213b008d9022b4c0e7c
Gustavo Boni <gustavo.boni@gmail.com>
2021-02-16 22:19:59 -0300

    camera adjustments android settings

---
+++ Assets/Scripts/CameraAdjustment.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAdjustment : MonoBehaviour
+{
+    public SpriteRenderer bg;
+
+    void Start()
+    {
+        float orthoSize = ((bg.bounds.size.x * Screen.height) / Screen.width) / 2;
+        GetComponent<Camera>().orthographicSize = orthoSize;
+    }
+}
